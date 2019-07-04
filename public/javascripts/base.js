542cb67e2edfa77b03ce96c5eb1410e1c4feb310
Chris De Gour <cdegour@cdegour-ltm01.internal.salesforce.com>
2019-07-04 08:03:25 -0700

    cool cool, we got custom events feeding now, just need to add some toasty goodness for this iteration

--- public/javascripts/base.js
+++ public/javascripts/base.js
@@ -1,89 +1,94 @@
+const cdcTopic = '/data/ChangeEvents';
+
 const eventedObjects = new Map();
 var newestMsg;
 
 const doc = document;
-    const socket = io();
-    socket.on('MESSAGE', (msg) => {
-      //const newBlerp = doc.createElement('li');
-      //newBlerp.append(msg);
-      //doc.getElementById('blerp').appendChild(newBlerp);
-      console.log(msg);
-      newestMsg = msg;
-      processNewEventSLDS(msg);
+const socket = io();
+
+//socket.io reactions
+socket.on(cdcTopic, (msg) => {
+    console.log(`new message on ${cdcTopic}`);
+    console.log(msg);
+    newestMsg = msg;
+    processNewCDCEvent(msg);
+});
+socket.on('PEMessage', (payload) => {
+    console.log(`new message on ${payload.eventType}`);
+    console.log(payload.message);
+    processNewCustomEvent(payload);
+});
+socket.on('NewSubSuccess', (payload) => {
+    console.log(`Confirmed success connecting to ${payload.topicName}`);
+    addTopicToList(payload.topicName);
+});
+socket.on('NewSubFailure', (payload) => {
+    console.log(`Failure connecting to ${payload.topicName}`);
+    console.log(payload.error);
 });
 
 
+//Platform Event Logic
 
-function processNewEvent(event){
-    let objectName = event.payload.ChangeEventHeader.entityName;
-    if(!eventedObjects.get(objectName)!= 1){
-        eventedObjects.add(objectName, 1);
-        //create new tab to hold object, todo.
-    }
-    let eventsContainer = doc.getElementById('mainContainer');
-    
-    //build containers
-    let txnLI = doc.createElement('li');
-    txnLI.setAttribute('data-txnID', event.payload.ChangeEventHeader.transactionKey);
-    let eventDiv = doc.createElement('div');
-    eventDiv.setAttribute('data-eventID', event.event.replayId);
-    let eventHeader = doc.createElement('div');
-    eventHeader.classList.add('eventHeader');
-    let eventBody = doc.createElement('div');
-    eventBody.classList.add('eventBody');
-
-    //build header
-    //set up title
-    let titleArea = doc.createElement('h3');
-    titleArea.append(`Change Event: ${event.payload.ChangeEventHeader.entityName} - ${event.payload.ChangeEventHeader.changeType}`);
-    eventHeader.appendChild(titleArea);
-    //set up subtitle
-    let subtitleArea = doc.createElement('h4');
-    subtitleArea.append(`Event Time: ${event.payload.LastModifiedDate}`);
-    eventHeader.appendChild(subtitleArea);
-    
-    //build body
-    //List of record IDs
-    let recordList = doc.createElement('ul');
-    recordList.classList.add('recordList');
-    event.payload.ChangeEventHeader.recordIds.forEach(recordId => {
-        let recordIDItem = doc.createElement('li');
-        recordIDItem.append(recordId);
-        recordList.appendChild(recordIDItem);
-    });
-    eventBody.appendChild(recordList);
-    //List of field changes
-    let fieldChangelist = doc.createElement('ul');
-    fieldChangelist.classList.add('fieldChangeList');
-    let fieldsChanged = Object.keys(event.payload);
-    fieldsChanged.forEach(field => {
-        if(field != 'LastModifiedDate' && field != 'ChangeEventHeader'){
-            let fieldItem = doc.createElement('li');
-            fieldItem.append(`${field}: ${event.payload[field]}`);
-            fieldChangelist.appendChild(fieldItem);
-        }
-    });
-    eventBody.appendChild(fieldChangelist);
-    let commitingUser = doc.createElement('h4');
-    commitingUser.classList.add('committingUser');
-    commitingUser.append(`User: ${event.payload.ChangeEventHeader.commitUser}`);
-    eventBody.appendChild(commitingUser);
-
-    eventDiv.appendChild(eventHeader);
-    eventDiv.appendChild(eventBody);
-    txnLI.appendChild(eventDiv);
-    eventsContainer.appendChild(txnLI);
+function addPESubscription(peName){
+    socket.emit('ADD PE SUB', peName);
 }
 
+function processNewCustomEvent(event){
+    let objectName = event.eventType;
+    event = event.message;
+    console.log(event);
+    
+    const peContainer = doc.getElementById('peContainer');
 
+    let newEventMarkup = `
+  <article class="slds-card" data-eventID="${event.event.replayId}">
+  <div class="slds-card__header slds-grid slds-grid_vertical">
+    ${handleEventHeader(objectName, 'New Event')}
+    <div class="slds-card__body slds-col">
+        <table class="fieldList slds-table slds-table-bordered slds-table_cell-buffer slds-no-row-hover slds-table_bordered slds-table_fixed-layout">
+            <thead>
+            <tr class="slds-line-height_reset">
+                <th scope="col">
+                <div class="slds-truncate" title="Field Name">Field Name</div>
+                </th>
+                <th scope="col">
+                <div class="slds-truncate" title="Value">Value</div>
+                </th>
+            </tr>
+            </thead>
+            <tbody>`;
+        //add field content
+        //List of field changes
+        let fieldsChanged = Object.keys(event.payload);
+        fieldsChanged.forEach(field => {
+            newEventMarkup += handleEventField(field, event.payload[field]);
+        });
+        newEventMarkup += `
+                </tbody>
+            </table>
+            </div>
+         <footer class="slds-card__footer slds-col">
+                <span>User ID: ${event.payload.CreatedById}</span><br/>
+                <span>Time: ${event.payload.CreatedDate}</span>
+         </footer>
+         </div></article>
+         `;
 
-function processNewEventSLDS(event){
+         let txnDiv = doc.createElement('div');
+         txnDiv.innerHTML= newEventMarkup;
+         peContainer.appendChild(txnDiv);
+}
+
+//Logic for actual things goes below here
+
+function processNewCDCEvent(event){
     let objectName = event.payload.ChangeEventHeader.entityName;
     if(!eventedObjects.get(objectName)!= 1){
         eventedObjects.add(objectName, 1);
         //create new tab to hold object, todo.
     }
-    const eventsContainer = doc.getElementById('mainContainer');
+    const cdcContainer = doc.getElementById('cdcContainer');
 
     let newEventMarkup = `
   <article class="slds-card" data-eventID="${event.event.replayId}">
@@ -149,7 +154,7 @@ function processNewEventSLDS(event){
          let txnDiv = doc.createElement('div');
          txnDiv.setAttribute('data-txnID', event.payload.ChangeEventHeader.transactionKey);
          txnDiv.innerHTML= newEventMarkup;
-         eventsContainer.appendChild(txnDiv);
+         cdcContainer.appendChild(txnDiv);
 }
 
 function handleEventField(fieldName, fieldValue){
@@ -205,7 +210,7 @@ function handleEventHeader(objectName, changeType){
     let imageHTML;
     objectName;
 
-    if(objectName.indexOf('__c') == -1){
+    if(objectName.indexOf('__c') == -1 && objectName.indexOf('__e') == -1){
         //standard object
         imageHTML = `
             <div class="slds-media__figure">
@@ -216,7 +221,7 @@ function handleEventHeader(objectName, changeType){
                     <span class="slds-assistive-text">${objectName}</span>
                 </span>
             </div>`;
-    }else{
+    }else if(objectName.indexOf('__e') == -1){
         //custom object
         imageHTML = `
             <div class="slds-media__figure">
@@ -227,31 +232,78 @@ function handleEventHeader(objectName, changeType){
                     <span class="slds-assistive-text">${objectName}</span>
                 </span>
             </div>`;
+    }else {
+        //custom Event
+        imageHTML = `
+            <div class="slds-media__figure">
+                <span class="slds-icon_container slds-icon-standard-apex-plugin" title="${objectName}">
+                    <svg class="slds-icon slds-icon_small" aria-hidden="true">
+                        <use xlink:href="/assets/icons/standard-sprite/svg/symbols.svg#apex_plugin"></use>
+                    </svg>
+                    <span class="slds-assistive-text">${objectName}</span>
+                </span>
+            </div>`;
     }
     let titleHTML = `
             <div class="slds-media__body">
                 <h2 class="slds-card__header-title">
-                    <a href="javascript:void(0);" class="slds-card__header-link slds-truncate" title="${objectName} - ${changeType}"><span>${objectName} - ${changeType}: # records touched</span></a>
+                    <a href="javascript:void(0);" class="slds-card__header-link slds-truncate" title="${objectName} - ${changeType}"><span>${objectName} - ${changeType}</span></a>
                 </h2>
             </div>`;
     return `${staticHeader}${imageHTML}${titleHTML}${staticFooter}`;
 }
+//simple page logic stuff goes here
 
-/* <header class="slds-media slds-media-center slds-col">
-      <div class="slds-media__figure">
-        <span class="slds-icon_container slds-icon-standard-contact" title="contact">
-            <svg class="slds-icon slds-icon_small" aria-hidden="true">
-                <use xlink:href="/assets/icons/standard-sprite/svg/symbols.svg#contact"></use>
-            </svg>
-            <span class="slds-assistive-text">contact</span>
-        </span>
-      </div>
-      <div class="slds-media__body">
-        <h2 class="slds-card__header-title">
-            <a href="javascript:void(0);" class="slds-card__header-link slds-truncate" title="${event.payload.ChangeEventHeader.entityName} - ${event.payload.ChangeEventHeader.changeType}"><span>${event.payload.ChangeEventHeader.entityName} - ${event.payload.ChangeEventHeader.changeType}: # records touched</span></a>
-        </h2>
-      </div>
-      <div class="slds-no-flex">
-      </div>
-    </header>
+function handleTabClick(event){
+    event.preventDefault();
+    let allTabs = document.getElementsByClassName('slds-is-active');
+    Array.from(allTabs).forEach(ele => {
+        ele.classList.remove('slds-is-active');
+        ele.parentElement.classList.remove('slds-has-focus');
+    });
+    let allPanes = document.getElementsByClassName('slds-show');
+    Array.from(allPanes).forEach(ele => {
+        ele.classList.remove('slds-show');
+        ele.classList.add('slds-hide');
+    });
+
+    let clickedTab = event.currentTarget;
+    clickedTab.classList.add('slds-is-active');
+    clickedTab.parentElement.classList.add('slds-has-focus');
+    let target = document.getElementById(clickedTab.getAttribute('aria-controls'));
+    target.classList.add('slds-show');
+    target.classList.remove('slds-hide');
+}
+function wakeToolTip(event){
+    console.log('waking?');
+    let targetTip = document.getElementById(event.currentTarget.getAttribute('aria-describedby'));
+    targetTip.classList.add('slds-rise-from-ground');
+    targetTip.classList.remove('slds-fall-into-ground');
+}
+
+function sleepToolTip(event){
+    console.log('sleeping?');
+    let targetTip = document.getElementById(event.currentTarget.getAttribute('aria-describedby'));
+    targetTip.classList.remove('slds-rise-from-ground');
+    targetTip.classList.add('slds-fall-into-ground');
+}
+
+function handlePESubmit(event){
+    event.preventDefault();
+    let subInput = document.getElementById('txtNewPESubscription');
+    let subName = subInput.value;
+    subInput.value= '';
+    addPESubscription(subName);
+}
+
+function addTopicToList(topicName){
+    const PEList = document.getElementById('subscribedPEList');
+    const newPEItem = document.createElement('div');
+    newPEItem.classList.add('subscribedPE', 'slds-col');
+    newPEItem.textContent= topicName;
+    PEList.appendChild(newPEItem);
+}
+
+/* Graveyard
+
     */
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 const nforce = require('nforce');
 const faye = require('faye');
-var subscription;
+
 
 //nforce's authentication to Salesforce org
 const org = nforce.createConnection({
@@ -16,13 +16,21 @@ const org = nforce.createConnection({
 });
 
 var fClient = undefined;
+const cdcTopic= '/data/ChangeEvents';
+
+
+const PESubscriptionList = new Map();
+var allCDCSubscription;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', {});
   if(fClient != undefined){
-    res.render('index', {});
+    console.log('current PESubscriptionList');
+    console.log(PESubscriptionList);
+    res.render('index', {eventSubList: Array.from(PESubscriptionList.keys())});
   }else{
+    console.log('fClient');
+    console.log(fClient);
     //need to auth
     res.redirect('/auth/sfdc');
   }
@@ -33,36 +41,66 @@ router.get('/auth/sfdc', (req, res) => {
 });
 
 router.get('/auth/sfdc/callback', (req, res) => {
-  org.authenticate({code: req.query.code}, (err, resp) => {
-    if(!err) {
-      console.log(`Access Token: ${req.access_token} : ${org.oauth.access_token}`);
-      fClient = new faye.Client(`${org.oauth.instance_url}/cometd/46.0`);
-      fClient.setHeader('Authorization', 'OAuth ' + org.oauth.access_token);
-      console.log('fClient connected?');
-      subscription = fClient.subscribe('/data/ChangeEvents', (message) => {
-        console.log('Event detected');
-        console.log(message);
-        req.app.io.emit('MESSAGE', message);
-      });
-      subscription.then(() => {
-        console.log('connected!');
-      })
-      .catch( (err) => {
-        console.error(err);
-      });
-    }
-    else console.log(`Error: ${err.message}`);
-  });
-  res.render('index', {});
+  if(fClient == undefined){
+    org.authenticate({code: req.query.code}, (err, resp) => {
+      if(!err) {
+        console.log(`Access Token: ${req.access_token} : ${org.oauth.access_token}`);
+        fClient = new faye.Client(`${org.oauth.instance_url}/cometd/46.0`);
+        fClient.setHeader('Authorization', 'OAuth ' + org.oauth.access_token);
+        allCDCSubscription = fClient.subscribe(cdcTopic, (message) => {
+          console.log(`Event detected on ${cdcTopic}`);
+          console.log(message);
+          req.app.io.emit(cdcTopic, message);
+        });
+        fClient.on('transport:up', () => {
+          console.log('transport is active');
+        });
+        fClient.on('transport:down', () => {
+          console.log('transport is down');
+        });
+        allCDCSubscription.then(() => {
+          console.log('connected!');
+        })
+        .catch( (err) => {
+          console.error(err);
+        });
+        res.redirect('/');
+      }
+      else {console.log(`Error: ${err.message}`);}
+    });
+  }
+  else{
+    res.redirect('/');
+  }
+  
 });
 
 module.exports = function(io){
   io.on('connection', function(socket){
     console.log('connection made');
+    socket.on('ADD PE SUB', (peName) => {
+      console.log(`ask to add ${socket.id} to sub to ${peName}`);
+      if(!PESubscriptionList.get(peName)){
+        let newPESub = fClient.subscribe(`/event/${peName}`, (message) => {
+          console.log(`Event detected on ${peName}`);
+          console.log(message);
+          io.emit('PEMessage', {eventType: `${peName}`, message:message});
+        });
+        newPESub.then(() => {
+          console.log('New PE Connected!');
+          PESubscriptionList.set(peName, 1);
+          io.emit('NewSubSuccess', {topicName:`${peName}`});
+        })
+        .catch((err) => {
+          console.log('FAILURE Connecting to new PE Topic');
+          console.error(err);
+          io.emit('NewSubFailure', {topicName:`${peName}`, error:err});
+        });
+      }else{
+        console.log('ALREADY ON IT BOSS');
+      }
+    });
   });
-  
-  
+
   return router;
-
-
 }
