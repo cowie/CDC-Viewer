7ec02597f357d6c80cdd259fb9240e544e03c502
Chris De Gour <cdegour@cdegour-ltm01.internal.salesforce.com>
2019-07-08 14:37:23 -0700

    ok now you can logout and see who you are because Mark was running into all manner of bugs

--- routes/index.js
+++ routes/index.js
@@ -13,11 +13,13 @@ const org = nforce.createConnection({
   redirectUri: 'http://localhost:3000/auth/sfdc/callback',
   environment: process.env.ENVIRONMENT,
   mode: 'single',
+  autoRefresh: true,
 });
 
 var fClient = undefined;
 const cdcTopic= '/data/ChangeEvents';
 
+var userInfo = undefined;
 
 const PESubscriptionList = new Map();
 var allCDCSubscription;
@@ -25,8 +27,17 @@ var allCDCSubscription;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   if(fClient != undefined){
-    console.log('fClient defined, so auth is likely good')
-    res.render('index', {eventSubList: Array.from(PESubscriptionList.keys())});
+    /*console.log('fClient defined, so auth is likely good')
+    console.log('Information on username being used:');
+    console.log(userInfo);*/
+    if(userInfo == undefined){
+      org.getIdentity({}, (err, qres) => {
+        userInfo = qres;
+        res.render('index', {eventSubList: Array.from(PESubscriptionList.keys()), userInfo});
+      });
+    }else{
+      res.render('index', {eventSubList: Array.from(PESubscriptionList.keys()), userInfo});
+    }
   }else{
     console.log('fClient not defined, need to auth into SFDC');
     res.redirect('/auth/sfdc');
@@ -34,32 +45,33 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/auth/sfdc', (req, res) => {
-  console.log('redirecting to SFDC Oauth page');
-  res.redirect(org.getAuthUri());
+  //console.log('redirecting to SFDC Oauth page');
+  res.redirect(org.getAuthUri({prompt:'login'}));
 });
 
 router.get('/auth/sfdc/callback', (req, res) => {
   if(fClient == undefined){
-    console.log('validating authentication to SFDC');
+    //console.log('validating authentication to SFDC');
     org.authenticate({code: req.query.code}, (err, resp) => {
       if(!err) {
-        console.log(`Access Token: ${req.access_token} : ${org.oauth.access_token}`);
+        //console.log(`Access Token: ${req.access_token} : ${org.oauth.access_token}`);
         fClient = new faye.Client(`${org.oauth.instance_url}/cometd/46.0`);
         fClient.setHeader('Authorization', 'OAuth ' + org.oauth.access_token);
+        org.getIdentity({}, (err, res) => userInfo = res);
         allCDCSubscription = fClient.subscribe(cdcTopic, (message) => {
-          console.log(`Event detected on ${cdcTopic}`);
-          console.log(message);
+          /*console.log(`Event detected on ${cdcTopic}`);
+          console.log(message);*/
           req.app.io.emit(cdcTopic, message);
         });
         fClient.on('transport:up', () => {
-          console.log('transport is active');
+          //console.log('transport is active');
         });
         fClient.on('transport:down', () => {
-          console.log('transport is down');
+          //console.log('transport is down');
           req.app.io.emit('FayeDisconnect', 'transport:down detected');
         });
         allCDCSubscription.then(() => {
-          console.log('connected!');
+          //console.log('connected!');
         })
         .catch( (err) => {
           console.error(err);
@@ -70,15 +82,27 @@ router.get('/auth/sfdc/callback', (req, res) => {
     });
   }
   else{
-    console.log('Authentication valid, redirecting to main app');
+    //console.log('Authentication valid, redirecting to main app');
     res.redirect('/');
   }
   
 });
 
+router.get('/revoke', (req, res) => {
+  //console.log(org.oauth);
+  org.revokeToken({token:org.oauth.access_token}, (oerr, ores) => {
+    if(oerr) console.log(oerr);
+    else{
+      console.log(org);
+    }
+    fClient = undefined;
+    res.redirect('/');
+  });
+});
+
 module.exports = function(io){
   io.on('connection', function(socket){
-    console.log('connection made');
+    //console.log('connection made');
     socket.on('ADD PE SUB', (peName) => {
       console.log(`ask to add ${socket.id} to sub to ${peName}`);
       if(!PESubscriptionList.get(peName)){
@@ -88,7 +112,7 @@ module.exports = function(io){
           io.emit('PEMessage', {eventType: `${peName}`, message:message});
         });
         newPESub.then(() => {
-          console.log('New PE Connected!');
+          //console.log('New PE Connected!');
           PESubscriptionList.set(peName, 1);
           io.emit('NewSubSuccess', {topicName:`${peName}`});
         })
